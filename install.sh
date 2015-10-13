fe497e085f0697cbcffe87914f4532a06fb2c70d
Matthew Marchese <maffblaster@gentoo.org>
2015-10-13 15:23:07 -0700

    Buffed install script.

--- install.sh
+++ install.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 
-# This script installs all templates into your user's ${HOME}/Template directory.
+# This POSIX compliant script installs all templates into the ${HOME}/Template directory of the user running the script.
 
-CURRENT_DIR=`pwd`
+# Bash shells have PWD available.
 
-if [[ -d ${HOME}/Templates ]]; then
-	printf "Huzzah! ${HOME}/Templates exists!"\\n
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+function _install {
+cp -vrf ${DIR}/templates/* ~/Templates || printf "Could not copy files. Do you have correct permissions?"\\n; exit 1
+}
+
+if [[ -e ${HOME}/Templates ]] && [[ -d ${HOME}/Templates ]]; then
+	printf "Found ${HOME}/Templates"\\n >> /dev/null
+	_install
+	exit 0
+elif [[ -e ${HOME}/Templates ]]; then
+	mkdir ${HOME}/Templates || printf "Could not create ${HOME}/Templates directory. Do you have correct permissions?"\\n; exit 1
+	_install
+	exit 0
 else
-	mkdir ${HOME}/Templates || printf "Could not create ~/Templates directory. Do you have correct permissions?"\\n	
+	if [[ -b ${HOME}/Templates ]] || [[ -c ${HOME}/Templates ]] || [[ -f ${HOME}/Templates ]]; then
+		printf "${HOME}/Templates seems to exist as file or special file."\\n\\n
+		printf "Please manually remove ${HOME}/Templates before running this script."\\n
+		exit 1
+	else
+		mkdir ${HOME}/Templates || printf "Could not create ${HOME}/Templates directory. Do you have correct permissions?"\\n
+		exit 1
+	fi
 fi
-cp -vrf ${CURRENT_DIR}/templates/* ~/Templates
-
-printf "Done!"\\n
